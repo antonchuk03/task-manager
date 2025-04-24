02a4eedd330caf91443763a94eb4c38efb1f38c4
antonchuk03 <antonchuk03@gmail.com>
2025-04-24 12:23:34 +0300

    add express & body-parser

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-const sum = (a, b) => a + b;
-console.log(sum(10, 5)); 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+//middleware
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
